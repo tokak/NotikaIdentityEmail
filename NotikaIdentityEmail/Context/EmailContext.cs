806d7c73a78a7214a5ed2c62a6d6aa0130f2d2b7
Murat Tokak <tokakmurat01@gmail.com>
2025-07-11 10:18:52 +0300

    Add project files.

---
+++ NotikaIdentityEmail/Context/EmailContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NotikaIdentityEmail.Entities;
+
+namespace NotikaIdentityEmail.Context
+{
+    public class EmailContext: IdentityDbContext<AppUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-141UAGI;initial catalog=NotikaEmailDb;integrated security=true;trust server certificate=true");
+        }
+    }
+}
---
+++ NotikaIdentityEmail/Controllers/RegisterController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using NotikaIdentityEmail.Entities;
+using NotikaIdentityEmail.Models;
+
+namespace NotikaIdentityEmail.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(RegisterUserViewModel model)
+        {
+            AppUser appUser = new AppUser()
+            {
+                Name = model.Name,
+                Surname = model.Surname,
+                Email = model.Email,
+                UserName = model.Username
+            };
+
+            var result = await _userManager.CreateAsync(appUser,model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("UserLogin","Login");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+                return View();
+        }   
+    }
+}
