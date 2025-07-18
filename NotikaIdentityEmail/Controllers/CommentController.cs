29e6e1b0092890c96ef5eb937bf266ea54769e1f
Murat Tokak <tokakmurat01@gmail.com>
2025-07-19 01:39:55 +0300

    Yapay Zeka ile toksik yorum analizi

--- NotikaIdentityEmail/Controllers/CommentController.cs
+++ NotikaIdentityEmail/Controllers/CommentController.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NotikaIdentityEmail.Context;
 using NotikaIdentityEmail.Entities;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 
 namespace NotikaIdentityEmail.Controllers
 {
@@ -39,8 +42,112 @@ namespace NotikaIdentityEmail.Controllers
             var user = await _userManager.FindByNameAsync(User.Identity.Name);
             comment.AppUserId = user.Id;
             comment.CommentDate = DateTime.Now;
-            comment.CommentStatus = "Onay Bekliyor";
-            _context.Comments.Add(comment);
+
+
+
+            using (var client = new HttpClient())
+            {
+                var apiKey = "Key anahtarınız";
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+
+                try
+                {
+                    var translateRequestBody = new
+                    {
+                        inputs = comment.CommentDetail
+                    };
+                    var translateJson = JsonSerializer.Serialize(translateRequestBody);
+                    var translateContent = new StringContent(translateJson, Encoding.UTF8, "application/json");
+
+                    var translateResponse = await client.PostAsync("https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-tr-en", translateContent);
+
+                    var translateResponseString = await translateResponse.Content.ReadAsStringAsync();
+
+                    string englishText = comment.CommentDetail;
+                    if (translateResponseString.TrimStart().StartsWith("["))
+                    {
+                        var translateDoc = JsonDocument.Parse(translateResponseString);
+                        englishText = translateDoc.RootElement[0].GetProperty("translation_text").GetString();
+                    }
+
+                    var toxicRequestBody = new
+                    {
+                        inputs = englishText
+                    };
+
+                    var toxicJson = JsonSerializer.Serialize(toxicRequestBody);
+                    var toxicContent = new StringContent(toxicJson, Encoding.UTF8, "application/json");
+
+                    var toxicResponse = await client.PostAsync("https://api-inference.huggingface.co/models/unitary/toxic-bert", toxicContent);
+
+                    var toxicResponseString = await toxicResponse.Content.ReadAsStringAsync();
+
+                    if (toxicResponseString.TrimStart().StartsWith("["))
+                    {
+                        var toxicDoc = JsonDocument.Parse(toxicResponseString);
+                        foreach (var item in toxicDoc.RootElement[0].EnumerateArray())
+                        {
+                            string label = item.GetProperty("label").GetString();
+                            double score = item.GetProperty("score").GetDouble();
+
+                            if (score > 0.5)
+                            {
+                                comment.CommentStatus = "Toksik Yorum";
+                                break;
+                            }
+                        }
+                    }
+                    if (string.IsNullOrEmpty(comment.CommentStatus))
+                    {
+                        comment.CommentStatus = "Onay Bekliyor";
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    comment.CommentStatus = "Onay Bekliyor";
+                }
+
+
+                _context.Comments.Add(comment);
+                _context.SaveChanges();
+                return RedirectToAction("UserCommentList");
+            }
+
+            
+        }
+
+        public IActionResult DeleteComment(int id)
+        {
+            var value = _context.Comments.Find(id);
+
+            _context.Comments.Remove(value);
+
+            _context.SaveChanges();
+            return RedirectToAction("UserCommentList");
+        }
+        public IActionResult CommentStatusChangeToToxic(int id)
+        {
+            var value = _context.Comments.Find(id);
+            value.CommentDetail = "Toksik Yorum";
+            _context.Comments.Update(value);
+            _context.SaveChanges();
+            return RedirectToAction("UserCommentList");
+        }
+        public IActionResult CommentStatusChangeToPassive(int id)
+        {
+            var value = _context.Comments.Find(id);
+            value.CommentDetail = "Yorum kaldırıldı";
+            _context.Comments.Update(value);
+            _context.SaveChanges();
+            return RedirectToAction("UserCommentList");
+        }
+        public IActionResult CommentStatusChangeToActive(int id)
+        {
+            var value = _context.Comments.Find(id);
+            value.CommentDetail = "Onaylandı";
+            _context.Comments.Update(value);
             _context.SaveChanges();
             return RedirectToAction("UserCommentList");
         }
